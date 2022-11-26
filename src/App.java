249fd5d9bc5013b9b25ca02031199ba9f21cf727
Simone Ciarlone <simoneciarlone@gmail.com>
2022-11-26 19:03:32 +0100

    nothing mutch

--- src/App.java
+++ src/App.java
@@ -15,56 +15,38 @@ public class App {
         ArrayList<GObject> objects = new ArrayList<GObject>();
         ArrayList<Light> lights = new ArrayList<Light>();
 
-        lights.add(new Light(new Transform(new Vector3(0, 0, 0)), 100));
+        lights.add(new Light(new Transform(new Vector3(1000, 1000, 100)), 100));
+        objects.add(new Sphere(new Transform(new Vector3(500, 600, 100)), new Material(new Color(255, 0, 0)), 100));
 
-        objects.add(new Sphere(new Transform(new Vector3(500, 500, 100)), new Material(new Color(255, 0, 0)), 100));
+        int w = 1000;
+        int h = 1000;
 
-        BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
+        BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 
-        for(int x = 0; x < 1000; x++){
-            for(int y = 0; y < 1000; y++){
-                Ray ray = new Ray(new Vector3(x, y, 0), new Vector3(0, 0, 1));
-                Vector3 intersection = null;
-                GObject object = null;
-                for(GObject o : objects){
-                    Vector3 i = o.getIntersection(ray);
-                    if(i != null){
-                        if(intersection == null){
-                            intersection = i;
-                            object = o;
-                        }else{
-                            if(Vector3.subtract(i, ray.getOrigin()).magnitude() < Vector3.subtract(intersection, ray.getOrigin()).magnitude()){
-                                intersection = i;
-                                object = o;
-                            }
-                        }
+        Vector3 lightDirection = new Vector3(1, -1, 0).normalize();
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                Ray ray = new Ray(new Vector3(i, h - j, 0), Vector3.FORWARD);
+                ArrayList<GObject> ObjectsInRay = new ArrayList<>();
+                for (GObject obj : objects) {
+                    Vector3 collisionPoint = obj.getIntersection(ray);
+                    if (collisionPoint != null) {
+                        Color c = obj.getMaterial(collisionPoint).getColor();
+                        ObjectsInRay.add(obj);
+                        Vector3 normal = obj.getNormal(collisionPoint);
+                        double p = Vector3.dot(normal, lightDirection);
+                        if (p < 0)
+                            p = 0;
+                        c = c.multiply(p);
+                        image.setRGB(i, j, c.getRGB());
                     }
                 }
-                if(intersection != null){
-                    Color color = new Color(0, 0, 0);
-                    for(Light light : lights){
-                        Vector3 lightDirection = Vector3.subtract(light.getTransform().getPosition(), intersection);
-                        Ray lightRay = new Ray(intersection, lightDirection);
-                        boolean blocked = false;
-                        for(GObject o : objects){
-                            if(o != object){
-                                if(o.getIntersection(lightRay) != null){
-                                    blocked = true;
-                                    break;
-                                }
-                            }
-                        }
-                        if(!blocked){
-                            double intensity = light.getIntensity() / (lightDirection.magnitude() * lightDirection.magnitude());
-                            color = Color.add(color, Color.multiply(object.getMaterial().getColor(), intensity));
-                        }
-                    }
-                    image.setRGB(x, y, color.getRGB());
-                }
+
             }
         }
 
         javax.imageio.ImageIO.write(image, "png", new java.io.File("image.png"));
-        
-    } 
+
+    }
 }
